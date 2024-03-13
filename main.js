f4740895a13696076a0cdc1d5372567c108f758f
Lenny Luo <76703996+Lennster1@users.noreply.github.com>
2024-03-13 15:29:47 -0400

    add js for printing numbers to screen and clear

--- main.js
+++ main.js
@@ -1,36 +1,55 @@
-function add(a, b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a, b){
-    return a - b;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a, b){
-    return a / b;
+function divide(a, b) {
+  return a / b;
 }
 
 let a, operator, b;
 function operate(x, operator, y) {
-    switch (operator) {
-        case "add":
-            add(x, y);
-            break;
-        case "subtract":
-            subtract(x, y);
-            break;
-        case "multiply":
-            multiply(x, y);
-            break;
-        case "divide":
-            divide(x, y);
-            break;
-    };
-};
+  switch (operator) {
+    case "add":
+      add(x, y);
+      break;
+    case "subtract":
+      subtract(x, y);
+      break;
+    case "multiply":
+      multiply(x, y);
+      break;
+    case "divide":
+      divide(x, y);
+      break;
+  }
+}
 
-let displayValue;
+let displayValue = [0];
 
+const buttons = document.querySelectorAll(".key");
+const clear = document.querySelector(".clear");
+const display = document.querySelector(".input");
+function updateDisplay() {
+  display.textContent = `${displayValue.join("")}`;
+}
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    displayValue.push(btn.id);
+    console.log(displayValue.join(""));
+    console.log(displayValue.length);
+
+    updateDisplay();
+  });
+});
+clear.addEventListener("click", () => {
+  displayValue = [];
+  updateDisplay();
+});
